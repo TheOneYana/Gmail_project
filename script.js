d29fd5bd2eb8fddbded5d37c11f1ff1352d5b646
TheOneYana <yanatheone@yahoo.com>
2022-04-04 10:26:52 -0700

    library updates

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 import { primary } from './primary.js';
-import { promotions } from './promotions';
-import { social } from './social';
+// import { promotions } from './promotions';
+// import { social } from './social';
 
+
+// const allInbox = [...primary, ...social, ...promotions];
+
+// module.exports = { primary, social, promotions, allInbox };
