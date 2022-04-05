5323b6a90e8d6c859425dc13274ff5c326d7fb97
aselwilliams <aselwilliams22@gmail.com>
2022-04-05 13:35:22 -0400

    Header is added. Still needs small changes

---
+++ Asel Gmail /all-emails.js
@@ -0,0 +1,10 @@
+// const primary = require('./primary');
+// const social = require('./social');
+// const promotions = require('./promotions');
+import { primary } from './primary.js';
+import { promotions } from './promotions.js';
+import { social } from './social.js';
+
+export const allInbox = [...primary, ...social, ...promotions];
+
+// module.exports = { primary, social, promotions, allInbox };
