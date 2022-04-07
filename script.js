48722f93c20d002e8bea768103ef0da87bd5782f
TheOneYana <yanatheone@yahoo.com>
2022-04-06 19:44:40 -0700

    width fixed

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function createEmail(emails) {
        <svg class="importantEmail" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15 7H7.89l3.57 5-3.57 5H15l3.55-5z" opacity=".3"/><path d="M16.63 5.84C16.27 5.33 15.67 5 15 5H4l5 7-5 6.99h11c.67 0 1.27-.32 1.63-.83L21 12l-4.37-6.16zM15 17H7.89l3.57-5-3.57-5H15l3.55 5L15 17z"/></svg>
      </div>
        <section class="sender">${emails[i].senderEmail} </section>
-       <section class="subject">${emails[i].messageTitle} </section> <span class="emailContent"> - Hello, Student!  Welcome to our bootcamp! You going...</span>
+       <section class="subject">${emails[i].messageTitle}<span class="emailContent"> - ${emails[i].messageTitle}</span></section> 
        </div>
        <div class = 'two'>
        <section class="time">${emailTime} </section>
